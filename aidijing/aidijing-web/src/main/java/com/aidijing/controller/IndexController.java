c1b89219733c9bf8328955d90957c5c61f6b0b0b
披荆斩棘 <yujunhao_8831@yahoo.com>
2017-05-26 17:21:56 +0800

    初始化提交

---
+++ aidijing/aidijing-web/src/main/java/com/aidijing/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.aidijing.controller;
+
+import org.springframework.boot.web.servlet.ErrorPageRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : 披荆斩棘
+ * @date : 2017/5/10
+ */
+@Controller
+public class IndexController {
+
+
+    /**
+     * @return
+     * @see com.aidijing.config.SpringConfig#registerErrorPages(ErrorPageRegistry)
+     */
+    @RequestMapping( "/404" )
+    public String errorPage404 () {
+        // 错误页面
+        return "redirect:https://yujunhao8831.github.io/";
+    }
+
+
+}
